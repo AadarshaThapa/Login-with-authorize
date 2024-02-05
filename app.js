ec9c64b2e7dfd6ac452d04ef60e89d7823a3bf79
Aadarsha <aadarshaandkazi@gmail.com>
2024-02-05 18:27:19 +0545

    Added a list table and reports page

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ myApp.config(['$routeProvider', function ($routeProvider) {
             templateUrl: 'Form.html',
             controller: 'DetailsController'
         })
+        .when('/reports', {
+            templateUrl: 'Reports.html',
+            controller: 'DashboardController'
+        })
         .otherwise({
             redirectTo: '/login'
         });
@@ -59,6 +63,34 @@ myApp.service('LoginService', function () {
 });
 
 myApp.controller('DashboardController', function ($scope, $location, LoginService, tilesService, indvTilesService) {
+
+    $scope.listtables = [
+        {
+            id: 1,
+            name: "Jenson Delaney",
+            email: "jsaf@mail.com",
+            messagesCount: 3,
+        },
+        {
+            id: 2,
+            name: "Amaya Coffey",
+            email: "amaya.coffey@mail.com",
+            messagesCount: 1,
+        },
+        {
+            id: 3,
+            name: "Habib Joyce",
+            email: "habib.joyce@mail.com",
+            messagesCount: 5,
+        },
+        {
+            id: 4,
+            name: "Lilly-Ann Roche",
+            email: "lilly-ann.roche@mail.com",
+            messagesCount: 8,
+        },
+        
+    ];
     $scope.tiles = tilesService.getTiles();
 
     $scope.navigateToDetails = function (tile) {
